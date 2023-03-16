cf02034b3709f0db5f4e96a2db62c53f2e255dfc
MarkMurasaki <mark.murasaki.chan@gmail.com>
2023-03-16 15:52:34 +0800

    qwe

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.get('/api/hello', function (req, res) {
 });
 
 app.get('/api/whoami',function(req,res){
-  
-  res.json({ipaddress:req.remoteAddress,language:req.headers["accept-language"],software:req.headers["user-agent"]});
+  console.log(req.socket.remoteAddress)
+  res.json({ipaddress:req.socket.remoteAddress,language:req.headers["accept-language"],software:req.headers["user-agent"]});
 });
 
 // listen for requests :)
